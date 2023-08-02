0f627f1680589cadf7be963e7cc4af3e0992e02a
pavansai <pavansaivvit@gmail.com>
2023-08-02 12:50:17 +0000

    one testcase failed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,4 @@
 import './index.css'
-import Loader from 'react-loader-spinner'
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import UserInfo from '../UserInfo'
 import BlogList from '../BlogList'
 
